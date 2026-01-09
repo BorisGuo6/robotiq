b55a834cdf371bd3fa5ab34e8dbdcc0236281423
BorisGuo <borisguo6@gmail.com>
2026-01-09 12:44:41 +0800

    Update Robotiq2FGripperSimpleController.py

--- robotiq_2f_gripper_control/nodes/Robotiq2FGripperSimpleController.py
+++ robotiq_2f_gripper_control/nodes/Robotiq2FGripperSimpleController.py
@@ -46,6 +46,11 @@ import rospy
 from robotiq_2f_gripper_control.msg import _Robotiq2FGripper_robot_output  as outputMsg
 from time import sleep
 
+try:
+    raw_input
+except NameError:
+    raw_input = input
+
 
 def genCommand(char, command):
     """Update the command according to the character entered by the user."""
